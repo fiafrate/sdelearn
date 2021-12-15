e7a8ac052f12569948ab68408ef26ec7e9087b63
fiafrate <francesco.iafrate@uniroma1.it>
2021-12-15 19:36:50 +0100

    Commit

--- tests/main-test.py
+++ tests/main-test.py
@@ -7,20 +7,70 @@
 import numpy as np
 
 from sdelearn import *
-# from sdelearn.sde_data import *
-# from sdelearn.sde_sampling import *
-# from sdelearn.sde_model import *
-# from sdelearn.sde_learner import *
-# from sdelearn.sde_qmle import*
-# from sdelearn.sde_lasso import*
-import sympy as sym
 
-import matplotlib.pyplot as plt
+import sympy as sym
 
 
 if __name__ == '__main__':
 
-    # symbol mode
+
+    # functional mode -------------------------
+
+    def b(x, param):
+        out = [0, 0]
+        out[0] = param["theta_dr00"] - param["theta_dr01"] * x[0]
+        out[1] = param["theta_dr10"] - param["theta_dr11"] * x[1]
+        return out
+
+
+
+    def A(x, param):
+        out = [[0, 0], [0, 0]]
+        out[0][0] = param["theta_di00"] + param["theta_di01"] * x[0]
+        out[1][1] = param["theta_di10"] + param["theta_di11"] * x[1]
+        out[1][0] = 0
+        out[0][1] = 0
+        return out
+
+
+    par_names = {"drift": ["theta_dr00", "theta_dr01", "theta_dr10", "theta_dr11"],
+                 "diffusion": ["theta_di00", "theta_di01", "theta_di10", "theta_di11"]}
+
+    sde = Sde(sampling=SdeSampling(initial=0, terminal=2, delta=0.01),
+              model=SdeModel(b, A, mod_shape=[2, 2], par_names=par_names, mode='fun'))
+
+    print(sde)
+
+
+    truep = {"theta_dr00": 0, "theta_dr01": -0.5, "theta_dr10": 0, "theta_dr11": -0.5, "theta_di00": 0, "theta_di01": 1,
+             "theta_di10": 0, "theta_di11": 1}
+    sde.simulate(truep=truep, x0=[1, 2])
+
+    sde.plot()
+
+    # qmle -----------------------------------
+
+    qmle = Qmle(sde)
+
+    # generate some random starting values
+    all_param = [p for k in par_names.keys() for p in par_names.get(k)]
+    n_param = len(all_param)
+    startp = dict(zip(all_param, np.round(np.abs(np.random.randn(n_param)), 1)))
+
+    qmle.fit(startp, method='BFGS')
+
+
+    qmle.est
+    qmle.vcov
+    qmle.optim_info
+
+
+    qmle.predict().plot()
+
+
+
+
+    # symbol mode --------------------
 
     n_var = 2
     theta_dr = [sym.symbols('theta_dr{0}{1}'.format(i, j)) for i in range(n_var) for j in range(2)]
